64dc8cd4276f039e32d0e3afda010fe42152717f
Clemente <dev@imkreative.com>
2022-12-05 09:12:21 -0500

    Add Import logic for cards

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 // import { DB } from "https://deno.land/x/sqlite/mod.ts";
-import { join } from "https://deno.land/std@0.165.0/path/mod.ts";
+import { join, resolve, toFileUrl } from "https://deno.land/std@0.165.0/path/mod.ts";
 import { init } from "./setup.ts";
 import { config, initConfig } from "./config.ts";
 import { addCard, getAll, getCard, getIDFromList, remove, update } from "./api.ts";
@@ -13,52 +13,23 @@ import {
   GradingCompany,
   MetadataType,
 } from "./types.ts";
+import { importCSV } from "./utils/import.ts";
 
 if (import.meta.main) {
   initConfig(join("data", "database.db"));
   init(config().DB_PATH);
 
-  const languages: CardLanguage[] = getAll("card_languages");
-  const editions: CardEdition[] = getAll("card_editions");
-  const conditions: CardCondition[] = getAll("card_conditions");
-  const sets: CardEdition[] = getAll("card_sets");
-  const companies: GradingCompany[] = getAll("grading_companies");
-  const marnie: CardSchema = {
-    name: "Marnie",
-    amount: 1,
-    price: 600,
-    cost: 0,
-    owned: true,
-    sold: false,
-    liquid: false,
-    spendable: false,
-    error: false,
-    graded: true,
-    condition: getIDFromList("Near Mint+", conditions),
-    language: getIDFromList("English", languages),
-    card_number: "4/100",
-    card_set: getIDFromList("Shiny Star V", sets),
-    edition: getIDFromList("Unlimited", editions),
-    grade: 9.5,
-    grading_company: getIDFromList("CGC", companies),
-    rarity: "SR",
-  };
+  await importCSV(new URL(toFileUrl(resolve(join("imported", "pokemon.csv")))))
 
-  addCard(marnie);
+  // console.log(getCard(1));
 
-  console.log(getCard(1));
+  // update(1, {
+  //   price: 400,
+  //   metadata_type: MetadataType.CARD,
+  //   metadata: {
+  //     grade: 9,
+  //   } as CardUpdate,
+  // } as AssetUpdate);
 
-  update(1, {
-    price: 400,
-    metadata_type: MetadataType.CARD,
-    metadata: {
-      grade: 9,
-    } as CardUpdate,
-  } as AssetUpdate);
-
-  console.log(getCard(1));
-
-  remove(1);
-
-  console.log(getCard(1));
+  // remove(1);
 }
---
+++ utils/import.ts
@@ -0,0 +1,53 @@
+import { parse } from "https://deno.land/std@0.167.0/encoding/csv.ts";
+import { addCard, getAll, getIDFromList } from "../api.ts";
+import { Account, CardCondition, CardEdition, CardLanguage, CardSchema, GradingCompany } from "./../types.ts";
+
+export async function importCSV(url: URL, account?: Account) {
+    const response = await fetch(url);
+    const csv = await response.text();
+    const data = parse(csv, {
+        skipFirstRow: true,
+        columns: [
+            'name', 'price', 'cost', 'amount', 'owned', 'sold', 'sold_price',
+            'liquid', 'spendable', 'card_set', 'edition', 'card_number', 'rarity',
+            'condition', 'error', 'graded', 'grading_company', 'grade', 'language'
+        ]
+    });
+    const languages: CardLanguage[] = getAll("card_languages");
+    const editions: CardEdition[] = getAll("card_editions");
+    const conditions: CardCondition[] = getAll("card_conditions");
+    const sets: CardEdition[] = getAll("card_sets");
+    const companies: GradingCompany[] = getAll("grading_companies");
+
+    data.forEach((row) => {
+        // const card
+        const card: CardSchema = {
+            name: row.name as string,
+            price: row.price as number,
+            cost: row.cost as number,
+            amount: row.amount as number,
+            owned: row.owned as boolean,
+            sold: row.sold as boolean,
+            sold_price: row.sold_price as number,
+            liquid: row.liquid as boolean,
+            spendable: row.spendable as boolean,
+            card_set: getIDFromList(row.card_set as string, sets),
+            edition: getIDFromList(row.edition as string, editions),
+            card_number: row.card_number as string,
+            rarity: row.rarity as string,
+            condition: getIDFromList(row.condition as string, conditions),
+            error: row.error as boolean,
+            graded: row.graded as boolean,
+            grading_company: getIDFromList(row.grading_company as string, companies),
+            language: getIDFromList(row.language as string, languages)
+        }
+
+        addCard(card);
+    })
+
+    // console.log(data, account);
+}
+
+export default {
+    importCSV
+}
